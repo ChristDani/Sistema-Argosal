cfa1bc1ad17f56e27d312e1e3a6343a72068cce9
Christian <christiand0393@gmail.com>
2022-11-22 20:19:37 -0500

    cambios 22/11/2022 (protegiendo rutas 5%)

--- controller/acceso/login.php
+++ controller/acceso/login.php
@@ -1,11 +1,9 @@
 <?php
 
-$_SESSION["user"]=null;
-
 require_once("../../model/usuarios.php");
 	
-$dni=$_POST['dni'];
-$clave=$_POST['clave'];
+$dni = $_POST['dni'];
+$clave = sha1($_POST['clave']);
 
 $consultas=new user();
 $filas2=$consultas->buscarUser($dni);
@@ -17,27 +15,27 @@ if ($filas2 != null)
 
 		$tdni = $columna[0];
 		$tusu=$columna[1];
-		$tclave = $columna[2];
+		$tclave = sha1($columna[2]);
 		$ttipo = $columna[3];
 			
 	}
 }
 else{
-	$tdni = '00000000';
-	$tclave = '000000';
+	header("location:../../index.php");
 }
 
 if(($dni==$tdni) && ($clave == $tclave))
 {
 
 	session_start();
+	$_SESSION["dni"]=$tdni;
 	$_SESSION["user"]=$tusu;
-	$activarEstado = $consultas->activarEstado($dni);
+	$_SESSION["tipo"]=$ttipo;
+	// $activarEstado = $consultas->activarEstado($dni);
 	header("location:../../index.php?pagina=Dashboard");
 
 
 }
-
 else
 {
 	header("location:../../index.php");
