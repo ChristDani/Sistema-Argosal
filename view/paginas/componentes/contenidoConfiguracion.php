66725f87ab282b50a54d41ab7f258bcd83916eed
Christian Daniel Campaña Díaz <christiand0393@gmail.com>
2022-11-24 17:50:35 -0500

    Cambios 24-11-22 Oliver

--- view/paginas/componentes/contenidoConfiguracion.php
+++ view/paginas/componentes/contenidoConfiguracion.php
@@ -1,1 +1,111 @@
 <h1>CONFIGURACIONES</h1>
+<div class="row">
+    <div class="col-xl-2 col-lg-2">
+        <div class=" card">
+            <div class="card-body d-flex flex-column">
+                <a class="" href="#list-item-1">Item 1</a>
+                <a class="" href="#list-item-2">Item 2</a>
+                <a class="" href="#list-item-3">Item 3</a>
+                <a class="mb-3" href="#list-item-4">Item 4</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-8 col-lg-8 mx-auto">
+        <div class="d-flex justify-content-between row">
+            <div class="col-xl-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h1>Usuario</h1>    
+                            <a href="#"><h3>Editar</h3></a>           
+                        </div>
+                        Apartir de aca que edite con el "a" de arriba
+                        <div class="row my-3 row-cols-lg-2">
+                            <div class="col d-flex justify-content-center align-items-center my-2">
+                                <img src="view/static/ProfileIMG/ProfileDefault.png" style="height:200px; border-radius:100px;">
+                            </div>
+                            <div class="col-xl-6 my-2 d-grid align-items-center">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="Nombre">
+                                    <label for="floatingInput">Nombre del usuario</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-cols-lg-2">
+                            <div class="col-xl-6">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Nueva Contraseña">
+                                    <label for="floatingPassword">Nueva contraseña</label>
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Confirmar contraseña">
+                                    <label for="floatingPassword">Confirmar contraseña</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary mb-3">Guardar Cambios</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h1>Tema</h1>
+                        </div>
+                        <div class="row d-flex flex-column">
+                            <h3>Elegir tema de preferencia</h3>
+                            <div class="align-items-end col d-flex justify-content-between mb-3">
+                                <button class="almrda Buttone mx-2 btn btn-link btn-sm d-flex justify-content-center">
+                                    <div class="color d-flex justify-content-center align-items-center">
+                                        <ion-icon name="moon-outline"></ion-icon>
+                                    </div>
+                                </button>    
+                                <button class="almrda Buttone mx-2 btn btn-link btn-sm d-flex justify-content-center">
+                                    <div class="color d-flex justify-content-center align-items-center">
+                                        <ion-icon name="moon-outline"></ion-icon>
+                                    </div>
+                                </button>                           
+                            </div>
+                            <h3>Elija el color que desea</h3>
+                            <div class="col d-flex justify-content-center mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mb-3">
+                                                <a href=""><div class="col danger-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col primary-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col success-bg" style="height:25px; border-radius:50px;"></div></a>
+                                            </div>
+                                            <div class="col mb-3">
+                                                <a href=""><div class="col primary-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col success-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col danger-bg" style="height:25px; border-radius:50px;"></div></a>
+                                            </div>    
+                                            <div class="col mb-3">
+                                                <a href=""><div class="col success-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col primary-bg mb-2" style="height:25px; border-radius:50px;"></div></a>
+                                                <a href=""><div class="col danger-bg" style="height:25px; border-radius:50px;"></div></a>
+                                            </div>                                   
+                                        </div>                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
