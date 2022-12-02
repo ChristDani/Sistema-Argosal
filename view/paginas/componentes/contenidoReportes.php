d6b735caa3ea6f072ec9c80dd401d5019d7b68a1
AgenteX <Oliverdg2@hotmail.com>
2022-12-02 13:33:32 -0500

    Hasta la madre x11

--- view/paginas/componentes/contenidoReportes.php
+++ view/paginas/componentes/contenidoReportes.php
@@ -22,10 +22,42 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-8">
+        <div class="col-lg-12 mb-3">
             <div class="card">
                 <div class="card-body">
-                    
+                    <table class="table table-responsive-xl color table-borderless">
+                        <thead class="table-dark">
+                            <tr>
+                            <th scope="col">N</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">Producto</th>
+                            <th scope="col">SEC</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Registro</th>
+                            <th scope="col">Detalles</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
