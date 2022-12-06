cfa0b3a5e84a89a556b74166707c4c4c55ea2342
Christian Daniel <christiand0393@gmail.com>
2022-12-06 02:39:42 -0500

    ytdyt

--- model/conexion.php
+++ model/conexion.php
@@ -5,19 +5,11 @@
 
 		public function conectar()
 		{			
-			// resto
-			// $server="localhost";
-			// $conexion=array("Database"=>"Argosal",
-			// 				"UID"=>"sa",
-			// 				"PWD"=>"123456",
-			// 				"CharacterSet"=>"UTF-8");
-
-			// christian
-			$server="192.168.0.233";
-			// $server="192.168.0.226";
+			
+			$server="localhost";
 			$conexion=array("Database"=>"Argosal",
-							"UID"=>"paraclase",
-							"PWD"=>"12345",
+							"UID"=>"sa",
+							"PWD"=>"123456",
 							"CharacterSet"=>"UTF-8");
 
 			$con=sqlsrv_connect($server,$conexion);
